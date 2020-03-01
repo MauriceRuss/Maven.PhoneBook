247b9ff025485511f0a9f1a978d90e1e1b97375f
Mo Russ <mauriceruss302@gmail.com>
2020-03-01 13:49:08 -0500

    cleaned code

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -12,10 +12,18 @@ public class PhoneBook  {
     private final Map<String, List<String>> phoneBook;
 
     //making the map in parameter the named map in field
-    //Copy constructor
+    //passed reference of map Copy constructor cloned original map
+    //added private method for null point exception in constuctor
     public PhoneBook(Map<String, List<String>> map) {
-        this.phoneBook = new LinkedHashMap<>(map);
+        this.phoneBook = new LinkedHashMap<>((map));
     }
+
+//    private Map<String, List<String>> validateMap(Map<String, List<String>> map) {
+//        for (Map.Entry<String,List<String>> numbers : phoneBook.entrySet()) {
+//            map.put(numbers.getKey(),numbers.getValue()==null? new ArrayList<>() : numbers.getValue());
+//        }
+//        return map;
+//    }
 
     //changing to LinkedHashMap for sorting issue to happen latter
     public PhoneBook() {
@@ -23,26 +31,18 @@ public class PhoneBook  {
     }
 
     public void add(String name, String phoneNumber) {
-        //ArrayList<String> actualPhoneNumbers = new ArrayList<>();
-        //actualPhoneNumbers.add(phoneNumber);
-        //this.phoneBook.put(name, actualPhoneNumbers);
         List<String> phoneNumbers;
         if (phoneBook.containsKey(name)) {
-          phoneNumbers = phoneBook.get(name);
+            phoneNumbers = phoneBook.get(name);
         }
         else {
             phoneNumbers = new ArrayList<String>();
             phoneBook.put(name,phoneNumbers);
         }
         phoneNumbers.add(phoneNumber);
-
     }
          
     public void addAll(String name, String... phoneNumbers) {
-        //ArrayList<String> actualPhoneNumbers = new ArrayList<>();
-        //for(String num : phoneNumbers){
-        //    actualPhoneNumbers.add(num);
-        //}
         this.phoneBook.put(name, Arrays.asList(phoneNumbers));
     }
 
@@ -51,10 +51,7 @@ public class PhoneBook  {
     }
 
     public Boolean hasEntry(String name) {
-          // if(this.phoneBook.containsKey(name) || this.phoneBook.containsValue(this.phoneNumber)){
-          //    return true;
-          // }
-          //return false;
+
         if(this.phoneBook.containsKey(name)) {
             return true;
         }
@@ -82,7 +79,6 @@ public class PhoneBook  {
     }
 
     public List<String> getAllContactNames() {
-        //ArrayList<String> contactNames = new ArrayList<String>(phoneBook.keySet());
         return new ArrayList<String>(phoneBook.keySet());
     }
 
