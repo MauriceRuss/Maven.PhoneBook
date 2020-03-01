0dcd22244fb2c9ddc404990b9cf04f93f417de9a
Mo Russ <mauriceruss302@gmail.com>
2020-03-01 13:28:16 -0500

    lab passes all test

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -6,34 +6,44 @@ import java.util.*;
  * Created by leon on 1/23/18.
  */
 public class PhoneBook  {
-    private String name;
-    private String phoneNumber;
-    private String[] phoneNumbers;
-    //giving field of a map for getter method for constructor test
-    private Map<String, List<String>> phoneBook;
 
+    //giving field of a map for getter method for constructor test
+    //final for immutability
+    private final Map<String, List<String>> phoneBook;
 
     //making the map in parameter the named map in field
+    //Copy constructor
     public PhoneBook(Map<String, List<String>> map) {
-        this.phoneBook = map;
+        this.phoneBook = new LinkedHashMap<>(map);
     }
+
     //changing to LinkedHashMap for sorting issue to happen latter
     public PhoneBook() {
         this.phoneBook = new LinkedHashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
-        ArrayList<String> actualPhoneNumbers = new ArrayList<>();
-        actualPhoneNumbers.add(phoneNumber);
-        this.phoneBook.put(name, actualPhoneNumbers);
+        //ArrayList<String> actualPhoneNumbers = new ArrayList<>();
+        //actualPhoneNumbers.add(phoneNumber);
+        //this.phoneBook.put(name, actualPhoneNumbers);
+        List<String> phoneNumbers;
+        if (phoneBook.containsKey(name)) {
+          phoneNumbers = phoneBook.get(name);
+        }
+        else {
+            phoneNumbers = new ArrayList<String>();
+            phoneBook.put(name,phoneNumbers);
+        }
+        phoneNumbers.add(phoneNumber);
+
     }
          
     public void addAll(String name, String... phoneNumbers) {
-        ArrayList<String> actualPhoneNumbers = new ArrayList<>();
-        for(String num : phoneNumbers){
-            actualPhoneNumbers.add(num);
-        }
-        this.phoneBook.put(name, actualPhoneNumbers);
+        //ArrayList<String> actualPhoneNumbers = new ArrayList<>();
+        //for(String num : phoneNumbers){
+        //    actualPhoneNumbers.add(num);
+        //}
+        this.phoneBook.put(name, Arrays.asList(phoneNumbers));
     }
 
     public void remove(String name) {
@@ -41,10 +51,21 @@ public class PhoneBook  {
     }
 
     public Boolean hasEntry(String name) {
-           if(this.phoneBook.containsKey(name) || this.phoneBook.containsValue(this.phoneNumber)){
-              return true;
-           }
-         return false;
+          // if(this.phoneBook.containsKey(name) || this.phoneBook.containsValue(this.phoneNumber)){
+          //    return true;
+          // }
+          //return false;
+        if(this.phoneBook.containsKey(name)) {
+            return true;
+        }
+        else {
+            for (Map.Entry<String,List<String>> numbers : phoneBook.entrySet()) {
+                if (numbers.getValue().contains(name)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
      //Needed for addAll test BE SURE TO LOOK AT TEST CAREFULLY
     public List<String> lookup(String name) {
@@ -61,13 +82,11 @@ public class PhoneBook  {
     }
 
     public List<String> getAllContactNames() {
-        ArrayList<String> contactNames = new ArrayList<String>(phoneBook.keySet());
-        
-        return contactNames;
+        //ArrayList<String> contactNames = new ArrayList<String>(phoneBook.keySet());
+        return new ArrayList<String>(phoneBook.keySet());
     }
 
     public Map<String, List<String>> getPhoneBook() {
-
         return phoneBook;
     }
 }
